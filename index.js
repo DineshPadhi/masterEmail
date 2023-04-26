db3b14ab7309700c0fb0297e457014afdd4f87cd
Mikuuu <dinesh.padhi@vernost.in>
2023-04-26 10:08:18 +0530

    added new api

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/add", (req, res) => {
+  res.send("Hello World! add");
+});
 
 app.listen(Port, () => {
   console.log("Server listening on port 3000");
