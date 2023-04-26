97f48f0aed32133011fb797062c9590f39962865
Pranay <pranayadavde15@gmail.com>
2023-04-26 11:54:45 +0530

    added divi

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.get("/min", (req, res) => {
 app.get("/plus", (req, res) => {
   res.send("Hello World! plus");
 });
+app.get("/div", (req, res) => {
+  res.send("Hello World! div");
+});
 app.listen(Port, () => {
   console.log("Server listening on port 3000");
 });
