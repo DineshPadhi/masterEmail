37093876b26d0f74c5e1966b03d7aa70d563d607
cpkushwaha9833 <cpkushwaha9833@gamil.com>
2023-04-26 14:56:26 +0530

    forms

---
+++ frontend/EmailCom/src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { FormComponent } from './form/form.component';
+
+
+@NgModule({
+  declarations: [
+    FormComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
