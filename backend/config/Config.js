ac9c0a45c741b249b2d3757a892ee3c2ae8c524f
Mikuuu <dinesh.padhi@vernost.in>
2023-04-28 10:51:13 +0530

    added dotenv

--- backend/config/Config.js
+++ backend/config/Config.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// require("dotenv").config();
+
+const configureDetails = {
+  port: process.env.PORT,
+  client: process.env.CLIENT,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  password: process.env.DB_PORT,
+  database: process.env.DATABASE,
+};
+module.exports = { configureDetails };
