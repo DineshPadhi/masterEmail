e4968c4e82daeedd155e457794b7c29fd3d337e3
Mikuuu <dinesh.padhi@vernost.in>
2023-04-28 16:13:40 +0530

    added search api

--- backend/model/Model.js
+++ backend/model/Model.js
@@ -7,8 +7,28 @@ const ShowData = () => {
   // console.log("data is", data);
   return knex("TemplateData").select("*");
 };
+const searchData = (searchCriteria) => {
+  // console.log("data is", data);
+  return knex("TemplateData")
+    .select("*")
+    .where("templateName", "diensh") //default single where clause
+    .where((qb) => {
+      if (searchCriteria.searchTerm) {
+        qb.where("templateName", "like", `%${searchCriteria.searchTerm}%`);
+      }
+
+      if (searchCriteria.itemType) {
+        qb.orWhere("items.itemType", "=", searchCriteria.itemType);
+      }
+
+      if (searchCriteria.category) {
+        qb.orWhere("items.category", "=", searchCriteria.category);
+      }
+    });
+};
 
 module.exports = {
   displayForm,
   ShowData,
+  searchData,
 };
