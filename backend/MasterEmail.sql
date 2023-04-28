11a1609f60fabc4fe8114ce27269da4d0d81c4c2
cpkushwaha9833 <cpkushwaha9833@gamil.com>
2023-04-28 10:52:26 +0530

    reset

--- backend/MasterEmail.sql
+++ backend/MasterEmail.sql
@@ -1,24 +1,28 @@
 
 create database MasterEmail;
+
 create table TemplateData(
     id int not null auto_increment primary key,
     templateName varchar(100) not null,
     templateCode varchar(50) not null,
-    scenario varchar(255) not null,
+    scenario varchar(255) null,
     providers varchar(100) not null,
     user varchar(100) not null,
     tier varchar(100) not null,
     emailType varchar(255) not null,
-    activity varchar(100) not null,
-    status varchar(10),
-    targetAudience varchar(255),
-    subject varchar(255),
-    body text
+    activity varchar(100) null,
+    status varchar(10) not null,
+    targetAudience varchar(255) not null,
+    subject varchar(255) not null,
+    body text not null
 );
+
 use MasterEmail;
-select *
-from TemplateData;
+
+select * from TemplateData;
+
 drop table TemplateData;
+
 INSERT INTO TemplateData (
         TemplateName,
         Template_Code,
--- frontend/EmailCom/src/app/home/form-service.service.ts
+++ frontend/EmailCom/src/app/home/form-service.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './../../../environment/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormServiceService {
+
+  url = environment.BS_BASE_URL
+
   constructor(private http: HttpClient) {}
 
   submitForm(data: any) {
@@ -23,7 +27,8 @@ export class FormServiceService {
       subject,
       body,
     } = data;
-    return this.http.post('http://localhost:3000/', {
+
+    return this.http.post(this.url, {
       templateName,
       templateCode,
       scenario,
