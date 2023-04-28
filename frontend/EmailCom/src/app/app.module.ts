dde4b4b4fe3844d4205d3603168cffecf5876990
cpkushwaha9833 <cpkushwaha9833@gamil.com>
2023-04-28 11:10:07 +0530

    btn disable

--- frontend/EmailCom/src/app/app.module.ts
+++ frontend/EmailCom/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../app/app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
