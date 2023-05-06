d23394a85ebe9cc376c5c6fd58dcc9c08e2a06e4
DineshPadhi <dinesh.padhi@vernost.in>
2023-05-06 23:07:09 +0530

    initial master changes

--- frontend/emailCom/src/app/app-routing.module.ts
+++ frontend/emailCom/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'createEmailTemplate',
+  pathMatch: 'full'
+},
+{
+  path: 'createEmailTemplate',
+  loadChildren: ()=> import('./home/home.module').then(m=> m.HomeModule)
+},
+{
+  path: 'allTemplateData',
+  loadChildren: ()=> import('../app/filter/filter/filter.module').then(m=> m.FilterModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
