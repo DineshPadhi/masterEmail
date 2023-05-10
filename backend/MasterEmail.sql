75d90e2ec2b7c04c462ed7f471c678055858aa80
Pranay <pranayadavde15@gmail.com>
2023-05-10 14:43:10 +0530

    mysql change

--- backend/MasterEmail.sql
+++ backend/MasterEmail.sql
@@ -1,9 +1,9 @@
 create database MasterEmail;
 
 create table TemplateData(
-    tamplate_id int  auto_increment primary key,
+    id int  auto_increment primary key,
     templateName varchar(100) not null,
-    templateCode varchar(50) not null ,
+    templateCode varchar(50) not null unique ,
     scenario varchar(255) null,
     providers varchar(100) not null,
     tier varchar(100) not null,
@@ -12,43 +12,40 @@ create table TemplateData(
     status varchar(10) not null,
     targetAudience varchar(255) not null,
     lang varchar(30) null
-);
+)ENGINE=InnoDB CHARACTER SET=utf8;
 insert into TemplateData(
 templateName,templateCode,scenario,providers,tier,emailType,activity,
 status,targetAudience,lang)
-values('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
+values('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'english, hindi',
 "com","+91",'eng'),
-('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
-"com","+91",'hin'),
-('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
-"com","+91",'mar');
+('xx',"happy_diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
+"com","+91",'hin');
+-- ('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
+-- "com","+91",'mar');
 use MasterEmail;
 select * from TemplateData;
 truncate table TemplateData;
 drop  table TemplateData;
 	
 create table lang(
-	lang_id int auto_increment primary key,
-    tamplate_id int,
+	id int auto_increment primary key,
+    tamplate_id int not null,
     templateCode varchar(50) not null,
     language varchar(30) null,
     subject varchar (255) not null,
     body text not null,
-	foreign key (tamplate_id) references TemplateData(tamplate_id)
-
+	foreign key (tamplate_id) references TemplateData(id)
     );
-insert into lang(
+insert into lang(tamplate_id,
 templateCode,language,subject,body)
 values
-("happy diwali","eng",'hello',"eng body"),
-("happy diwali","hin",'namaste',"hindi body"),
-("happy diwali","mar",'marathi',"marathi body");
+(5,"happy diwali","mar",'english',"eng body");
 
 
 truncate table lang;
 select * from lang;	
 drop  table lang;
 
-select lang.language,subject,body from lang inner join 
-TemplateData on lang.templateCode = TemplateData.templateCode where 
-TemplateData.lang = 'hin';
+select lang.tamplate_id, lang.templateCode, lang.subject, lang.body from lang inner join 
+TemplateData on lang.tamplate_id = TemplateData.id where 
+lang.language = 'hin';
