ebd5b16e199fe771e84c38ceb34a575abb1b16dc
Mikuuu <dinesh.padhi@vernost.in>
2023-05-11 15:31:08 +0530

    changed masterSQL

--- backend/MasterEmail.sql
+++ backend/MasterEmail.sql
@@ -1,9 +1,8 @@
 create database MasterEmail;
-
 create table TemplateData(
-    id int  auto_increment primary key,
+    id int auto_increment primary key,
     templateName varchar(100) not null,
-    templateCode varchar(50) not null unique ,
+    templateCode varchar(50) not null unique,
     scenario varchar(255) null,
     providers varchar(100) not null,
     tier varchar(100) not null,
@@ -12,40 +11,69 @@ create table TemplateData(
     status varchar(10) not null,
     targetAudience varchar(255) not null,
     lang varchar(30) null
-)ENGINE=InnoDB CHARACTER SET=utf8;
+) ENGINE = InnoDB CHARACTER SET = utf8;
 insert into TemplateData(
-templateName,templateCode,scenario,providers,tier,emailType,activity,
-status,targetAudience,lang)
-values('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'english, hindi',
-"com","+91",'eng'),
-('xx',"happy_diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
-"com","+91",'hin');
+        templateName,
+        templateCode,
+        scenario,
+        providers,
+        tier,
+        emailType,
+        activity,
+        status,
+        targetAudience,
+        lang
+    )
+values(
+        'xx',
+        "happy diwali",
+        "sce",
+        '87483311212',
+        "pranay@gmail.com",
+        "+91",
+        'english, hindi',
+        "com",
+        "+91",
+        'eng'
+    ),
+    (
+        'xx',
+        "happy_diwali",
+        "sce",
+        '87483311212',
+        "pranay@gmail.com",
+        "+91",
+        'enghh',
+        "com",
+        "+91",
+        'hin'
+    );
 -- ('xx',"happy diwali","sce",'87483311212',"pranay@gmail.com","+91",'enghh',
 -- "com","+91",'mar');
 use MasterEmail;
-select * from TemplateData;
+select *
+from TemplateData;
 truncate table TemplateData;
-drop  table TemplateData;
-	
+drop table TemplateData;
 create table lang(
-	id int auto_increment primary key,
-    tamplate_id int not null,
+    id int auto_increment primary key,
+    template_id int not null,
     templateCode varchar(50) not null,
-    language varchar(30) not null default('English'),
+    language varchar(30) not null,
     subject varchar (255) not null,
     body text not null,
-	foreign key (tamplate_id) references TemplateData(id)
-    );
-insert into lang(tamplate_id,
-templateCode,subject,body)
-values
-(3,"happy diwali",'aksa',"english body");
-
-
+    foreign key (template_id) references TemplateData(id)
+) ENGINE = InnoDB CHARACTER SET = utf8;
+insert into lang(template_id, templateCode, language, subject, body)
+values (2, "happy diwali", 'English', 'aksa', "english body");
 truncate table lang;
-select * from lang;	
-drop  table lang;
-
-select lang.tamplate_id, lang.templateCode, lang.subject, lang.body from lang inner join 
-TemplateData on lang.tamplate_id = TemplateData.id where 
-lang.language = 'hin';
+select *
+from lang;
+drop table lang;
+select lang.tamplate_id,
+    lang.templateCode,
+    lang.subject,
+    lang.body
+from lang
+    inner join TemplateData on lang.tamplate_id = TemplateData.id
+where lang.language = 'hin';
