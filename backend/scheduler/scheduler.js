2fb499238226d79b81fec414028de00c988650e8
Mikuuu <dinesh.padhi@vernost.in>
2023-05-18 12:31:14 +0530

    cron

--- backend/scheduler/scheduler.js
+++ backend/scheduler/scheduler.js
@@ -1,38 +1,36 @@
-const cron = require('node-cron')
-const configureDetails = require('../config/Config.js');
-const nodeMailer = require('nodemailer')
+const cron = require("node-cron");
+const configureDetails = require("../config/Config.js");
+const nodeMailer = require("nodemailer");
 
-async function providers(name, sub,body) {
+async function providers(name, sub, body) {
+  console.log("Cron Exicuted");
 
-    let send = nodeMailer.createTransport({
-        service:'gmail',
-        auth: {
-          user:configureDetails.configureDetails.email,
-          pass:configureDetails.configureDetails.pass
-        }
-      });
-  
-      let mailDetails = {
-        from: 'cpkushwaha9833@gmail.com',
-        to: name,
-        subject: sub,
-        text: body
-      }
-  
-    await send.sendMail(mailDetails, (err, data)=>{
+  let send = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: configureDetails.configureDetails.email,
+      pass: configureDetails.configureDetails.pass,
+    },
+  });
 
-        if(err){
-          console.log('can not send email');
-        }
-        else{
-          console.log('Email send Successfully');
-        }
-      })
-    
+  let mailDetails = {
+    from: "cpkushwaha9833@gmail.com",
+    to: name,
+    subject: sub,
+    text: body,
+  };
+
+  await send.sendMail(mailDetails, (err, data) => {
+    if (err) {
+      console.log("can not send email");
+    } else {
+      console.log("Email send Successfully");
+    }
+  });
 }
 
 cron.schedule("0 */2 * * * *", providers);
 
 module.exports = {
-    providers
-}
+  providers,
+};
